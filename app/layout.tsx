fb57a3e382541c9b0fa9bceac6fadca7ac9e9a12
code-sharad <codesharad@gmail.com>
2025-09-05 15:36:32 +0530

    removed gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,8 +38,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative dark:bg-gray-950 dark:text-gray-50 dark:text-opacity-90  mx-auto `}
       >
-        <div className="bg-[#b9afff]  fixed opacity-40 top-[12rem] -z-08 right-[5rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.7rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#fbb183b9]  fixed opacity-40 top-[1rem] -z-08 right-[9rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[14rem] sm:w-[68.7rem] dark:bg-[#ffc1b0]"></div>
+        {/* <div className="bg-[#b9afff]  fixed opacity-40 top-[12rem] -z-08 right-[5rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.7rem] dark:bg-[#946263]"></div> */}
+        {/* <div className="bg-[#fbb183b9]  fixed opacity-40 top-[1rem] -z-08 right-[9rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[14rem] sm:w-[68.7rem] dark:bg-[#ffc1b0]"></div> */}
         <ActiveSessionContextProvider>
           <Header />
           <CloudBG>
